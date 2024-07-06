59d3c2ec0ab2128acefa1cc1a5c4184b40852e25
SergeyBildanov <troktrokov@gmail.com>
2024-07-06 21:21:40 +0400

    first init

---
+++ src/js/generators.js
@@ -0,0 +1,31 @@
+/**
+ * Формирует экземпляр персонажа из массива allowedTypes со
+ * случайным уровнем от 1 до maxLevel
+ *
+ * @param allowedTypes массив классов
+ * @param maxLevel максимальный возможный уровень персонажа
+ * @returns генератор, который при каждом вызове
+ * возвращает новый экземпляр класса персонажа
+ *
+ */
+import Team from "./Team.js"
+export function* characterGenerator(allowedTypes, maxLevel) {
+  // TODO: write logic here
+  yield new allowedTypes[Math.floor(Math.random()*allowedTypes.length)](maxLevel);
+}
+
+/**
+ * Формирует массив персонажей на основе characterGenerator
+ * @param allowedTypes массив классов
+ * @param maxLevel максимальный возможный уровень персонажа
+ * @param characterCount количество персонажей, которое нужно сформировать
+ * @returns экземпляр Team, хранящий экземпляры персонажей. Количество персонажей в команде - characterCount
+ * */
+export function generateTeam(allowedTypes, maxLevel, characterCount) {
+  // TODO: write logic here
+  let characters = [];
+  for(let i=0; i<characterCount; i++){
+    characters.push(characterGenerator(allowedTypes, maxLevel).next().value);
+  }
+  return new Team(characters);
+}
